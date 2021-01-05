c87e034fa85c2aa53b333ed3b81ddc458b0fe135
clementRbl <clement.rbl@gmail.com>
2021-01-05 17:21:08 +0100

    add calendar in card

--- src/pages/Techniciens.vue
+++ src/pages/Techniciens.vue
@@ -18,11 +18,13 @@
     
   <div class="container-card">
 
-    <q-card  class="my-card q-ma-xl" 
-      v-for="(technicien, index) in techniciens"
-      :key="index"
-      :style="{'backround-image' : 'url('+require('../assets/pictures/'+technicien.image[0])+')'}">
-      
+    <q-card 
+      v-for="technicien in techniciens"
+      :key="technicien.id"
+      class="my-card q-ma-xl" 
+      >
+      <q-img :src="technicien.images" /> 
+
 
       <q-card-section>
         <q-btn
@@ -58,12 +60,21 @@
       <q-separator />
 
       <q-card-actions>
-        <q-btn flat round icon="event" />
-        <q-btn flat color="primary">
+        <q-btn flat @click="calendar = true" round icon="event" />
+        <q-btn flat  color="primary">
           Programmer une intervention
         </q-btn>
       </q-card-actions>
     </q-card>
+
+  <q-dialog v-model="calendar" persistent>
+    <q-date v-model="proxyDate">
+      <div class="row items-center justify-end q-gutter-sm">
+        <q-btn label="Cancel" color="primary" flat v-close-popup />
+        <q-btn label="OK" color="primary" flat @click="save" v-close-popup />
+      </div>
+    </q-date>
+</q-dialog>
 
   </div>
 
@@ -73,7 +84,7 @@
 <style lang="scss">
   .background-header {
    width: 100%;
-   margin: 0;
+   margin: 0%;
    background-color: rgb(236, 231, 231);
    height: 30vh;
    display: flex;
@@ -99,92 +110,115 @@
 
 <script>
 export default {
-  data() {
+    name: 'Techniciens',
+    data() {
      return {
       techniciens: [
         {
+          id: 1,
           name: 'Steven Spielberg',
           location: '1.3 km',
           stars: 5,
           speciality: 'Plombier',
           description: 'Au secours ça coule ! Bon, tant pis..',
-          images: '~assets/pictures/avatar1.png'
+          images: 'https://nsa40.casimages.com/img/2021/01/05/mini_210105032430360383.png'
         },
-        {
+         {
+          id: 2,
           name: 'Marlon Brando',
           location: '12.3 km',
           stars: 5,
           speciality: 'Électricien',
           description: '10 ans de coups de jus à son service !',
-          images: '~assets/pictures/avatar2.png'
+          images: 'https://nsa40.casimages.com/img/2021/01/05/mini_210105032430428847.png'
         },
         {
+          id: 3,
           name: 'Robert De Niro',
           location: '0.3 km',
           stars: 5,
           speciality: 'Carreleur',
           description: 'Ne le laissez pas sur le carreau..',
-          images: '~assets/pictures/avatar3.png'
+          images: 'https://nsa40.casimages.com/img/2021/01/05/mini_210105032430690936.png'
         },
         {
+          id: 4,
           name: 'Al Pacino',
           location: '27.6 km',
           stars: 5,
           speciality: 'Maçon',
           description: 'Truelle à la main, ne faites pas les malins',
-          images: '~assets/pictures/avatar4.png'
+          images: 'https://nsa40.casimages.com/img/2021/01/05/mini_210105032430943151.png'
         },
         {
+          id: 5,
           name: 'Margot Robbie',
           location: '5.8 km',
           stars: 5,
           speciality: 'Menuisier',
           description: 'Margot adore collectionner les copeaux de bois',
-          images: '~assets/pictures/avatar5.png'
+          images: 'https://nsa40.casimages.com/img/2021/01/05/mini_210105032431200255.png'
         },
         {
+          id: 6,
           name: 'Scarlett Johansson',
           location: '4.6 km',
           stars: 5,
           speciality: 'Serrurier métallier',
           description: 'Clic, un coffre de plus ouvert !',
-          images: '~assets/pictures/avatar6.png'
+          images: 'https://nsa40.casimages.com/img/2021/01/05/mini_210105032431451308.png'
         },
         {
+          id: 7,
           name: 'Keanu Reeves',
           location: '3.2 km',
           stars: 5,
           speciality: 'Géomètre',
           description: 'La géométrie est son ..euh non !',
-          images: '~assets/pictures/avatar7.png'
+          images: 'https://nsa40.casimages.com/img/2021/01/05/mini_210105032431705434.png'
         },
         {
+          id: 8,
           name: 'Johnny Depp',
           location: '0.2 km',
           stars: 5,
           speciality: 'Charpentier',
           description: 'Maitre du haschi charpentier depuis des lustres...',
-          images: '~assets/pictures/avatar8.png'
+          images: 'https://nsa40.casimages.com/img/2021/01/05/mini_210105032431966916.png'
         },
         {
+          id: 9,
           name: 'Denzel Washington',
           location: '5.5 km',
           stars: 5,
           speciality: 'Peintre',
           description: 'Professionnel du barbouillage rapide',
-          images: '~assets/pictures/avatar9.png'
+          images: 'https://nsa40.casimages.com/img/2021/01/05/mini_210105032432249324.png'
         },
         {
+          id: 10,
           name: 'Ray Liotta',
           location: '2.6 km',
           stars: 5,
           speciality: 'Enseigniste',
           description: 'Quand ça clignote, c\'est tout bon !',
-          images: '~assets/pictures/avatar10.png'
-        },
+          images: 'https://nsa40.casimages.com/img/2021/01/05/mini_210105032432513306.png'
+        }, 
       ],
-      stars
+      model1: 'Sunday, Apr 28, 2019',
+      calendar: false,
+      date: '2019/03/01',
+      proxyDate: '2019/03/01'
       
+    }
+  },
+  methods: {
+    updateProxy () {
+      this.proxyDate = this.date
+    },
+
+    save () {
+      this.date = this.proxyDate
     }
   }
 }
